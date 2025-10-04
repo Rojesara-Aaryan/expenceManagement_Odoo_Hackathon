52f7004d3bd7e475f39e26b6e119b563ab1af1c7
Rojesara-Aaryan <aryan.rojesara@gmail.com>
2025-10-04 17:08:46 +0530

    web

--- expensemanagement_web/src/App.js
+++ expensemanagement_web/src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Welcome from './components/Welcome';
+import './index.css'; // Make sure this import is here
+import ForgotPass from "./components/ForgotPass";
+import Dashboard from "./components/Dashboard";
+import Uploadsection from "./components/Uploadsection";
+import EmployDetails from "./components/EmployDetails";
+import Employesview from "./components/Employesview";
+import Summerybar from "./components/Summerybar";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        {/* Default landing page */}
+        <Route path="/" element={<Welcome />} />
+        <Route path="/Dashboard" element={<Dashboard />} />
+        {/* Forgot password page */}
+        <Route path="/forgot-password" element={<ForgotPass />} />
+
+        {/* Employee main dashboard */}
+        <Route path="/employee" element={<Employesview />} />
+
+        {/* Individual subcomponents (for direct access/testing) */}
+        <Route path="/upload" element={<Uploadsection />} />
+        <Route path="/new" element={<EmployDetails />} />
+        <Route path="/summary" element={<Summerybar />} />
+      </Routes>
+    </Router>
   );
 }
 
